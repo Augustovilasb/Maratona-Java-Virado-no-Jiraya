52314bbb0bddcc676f3e7730f8761d6b730bffc5
Augusto <augustovilasb@hotmail.com>
2026-04-01 00:11:23 +0100

    feat: explain static vs instance initialization blocks
    - Static block executes once when the class is loaded
    - Instance block executes every time a new object is created
    - Position in the class does not change execution order
    - Useful for initializing class-level or object-level data

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Anime.java
@@ -0,0 +1,43 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
+
+public class Anime {
+
+    private String nome;
+    private static int[] episodios;
+
+    static {
+        System.out.println("Dentro do bloco de inicialização");
+        episodios = new int[10];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    public Anime(String nome) { this.nome = nome; }
+
+    public Anime() {
+        for (int episodio : Anime.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println(" ");
+    }
+
+        public String getNome () {
+        return nome;
+    }
+
+        public void setNome (String nome){
+        this.nome = nome;
+    }
+
+        public int[] getEpisodios () {
+        return episodios;
+    }
+
+        public void setEpisodios ( int qntEpisodios){
+        Anime.episodios = new int[qntEpisodios];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+    }
