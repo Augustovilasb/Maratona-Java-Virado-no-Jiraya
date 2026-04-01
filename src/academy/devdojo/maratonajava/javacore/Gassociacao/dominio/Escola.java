e37b79c246e2335d8f05f66cef955c47b5cd40fb
Augusto <augustovilasb@hotmail.com>
2026-04-01 17:05:40 +0100

    I create the Professor class and declare private Professor[] professores in the Escola class. Unlike the previous example where Jogador had one Time (one-to-one), here Escola has many Professor objects (one-to-many), which is why we use an array instead of a single object. This is still a unidirectional association because only Escola knows Professor, but Professor does not know Escola.

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Escola.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
+
+public class Escola {
+
+    //Relacionamento unidirecional
+    // Escolas podem ter muitos professores
+    // Mas um professor trabalha para uma Escola
+
+    private String nome;
+    private Professor[] professores;
+
+    public Escola(String nome, Professor[] professores) {
+        this.nome = nome;
+        this.professores = professores;
+    }
+
+    public void imprime() {
+        System.out.println("Escola " + this.nome);
+        if (professores == null) return;
+        for (Professor professor : professores) {
+            System.out.println(professor.getNome());
+        }
+    }
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+    public Professor[] getProfessores() {return professores;}
+    public void setProfessores(Professor[] professores) {this.professores = professores;}
+}
